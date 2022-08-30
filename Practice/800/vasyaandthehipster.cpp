0440bbec1653354493ef2a86c6e4421748527fe6
Luke Tan <luke@Lukes-MacBook-Air.local>
2022-08-31 01:23:01 +0800

    Codeforces Round 817 Div 4

--- Practice/800/vasyaandthehipster.cpp
+++ Practice/800/vasyaandthehipster.cpp
@@ -20,7 +20,7 @@ typedef long double ld;
 using namespace std;
 
 int main(){
-  #ifndef ONLINE_JUDGE
+  #ifndef ONLINE_JUDGE 
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
   #endif
