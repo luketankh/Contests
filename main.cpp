1e5f068d600846b9986fd494037f47ffe5dbdc4a
Luke Tan <luke@Lukes-MacBook-Air.local>
2023-02-07 23:45:05 +0800

    update

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <bits/stdc++.h>
 
 // typedef shortcuts 
@@ -9,24 +10,49 @@ typedef long double ld;
 
 using namespace std;
 
+bool IsPrime(ll n)
+{
+    if (n == 2 || n == 3)
+        return true;
+
+    if (n <= 1 || n % 2 == 0 || n % 3 == 0)
+        return false;
+
+    for (int i = 5; i * i <= n; i += 6)
+    {
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+    }
+
+    return true;
+}
+
+
 int main(){
-  #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin); 
     freopen("output.txt", "w", stdout);
-  #endif
+    #endif
     int cas;
     cin >> cas;
     while (cas--){
-      int n; cin >> n;
-      vector<int> v;
-      set<int> s;
-      for (int i = 0; i < n; i++){
-        ll a = 0;
-        cin >> a;
-        v.push_back(a);
-        s.insert(a);
-      }
-      if (s.size() < v.size()) cout << "NO" << '\n';
-      else cout << "YES" << '\n';
+    int n = 3;
+    int a[n];
+    int c_n = 0;
+    int c_z = 0;
+    for (int i = 0; i < n; ++i)
+    {    
+        cin >> a[i];
+        if (a[i] < 0) c_n ++;
+        else if (a[i] == 0) c_z++;
+    }
+    
+    if (c_n == 3) cout << "NO" << '\n';
+    else if (c_n == 2 && c_z == 1) cout << "NO" << '\n';
+    else if (c_n == 1 && c_z == 2 ) cout << "NO" << '\n';
+    else if (c_n == 0 && c_z == 2 ) cout << "NO" << '\n';
+    else if (c_n == 0 && c_z == 0 ) cout << "NO" << '\n';
+    else if (c_z== 3) cout << "NO" << '\n';
+    else cout << "YES" << '\n';
     }
 }
